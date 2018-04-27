949be6baf5fb348f7a28ab9fe3968002dd04a60c
Attila Kántor <attila.kantor@ericsson.com>
2018-04-27 09:47:29 +0200

    Added fixtures for start and stop sh

--- Backend/closet/start.sh
+++ Backend/closet/start.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 ps aux | grep closet-0.0.1-SNAPSHOT.jar | awk '{print $2}' | xargs kill -9
 export BUILD_ID=dontKillMeJenkins
 nohup java -jar target/closet-0.0.1-SNAPSHOT.jar &
--- Backend/closet/stop.sh
+++ Backend/closet/stop.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 ps aux | grep closet-0.0.1-SNAPSHOT.jar | awk '{print $2}' | xargs kill -9
