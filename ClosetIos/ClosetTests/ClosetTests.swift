1630fb3c01c49fa020a3f6172a6f8628a2a02826
kissLevi <kentaur@inf.elte.hu>
2018-04-27 22:01:00 +0200

    few improvements

---
+++ ClosetIos/ClosetTests/ClosetTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClosetTests.swift
+//  ClosetTests
+//
+//  Created by Kiss Levente on 2018. 04. 26..
+//  Copyright © 2018. Kiss Levente. All rights reserved.
+//
+
+import XCTest
+@testable import Trax
+
+class ClosetTests: XCTestCase {
+    
+  
+    
+}
