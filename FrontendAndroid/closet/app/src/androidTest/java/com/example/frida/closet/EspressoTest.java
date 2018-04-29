f7b97fa5e60732c1450426b9713a557670e43c34
mfridaa <mayer.friderika@gmail.com>
2018-04-29 08:52:10 +0200

    espresso test and new toilet display

--- FrontendAndroid/closet/app/src/androidTest/java/com/example/frida/closet/EspressoTest.java
+++ FrontendAndroid/closet/app/src/androidTest/java/com/example/frida/closet/EspressoTest.java
@@ -8,10 +8,17 @@ import org.junit.runner.RunWith;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.text.InputType;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withInputType;
+import static android.support.test.espresso.matcher.ViewMatchers.withTagKey;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
@@ -23,7 +30,10 @@ public class EspressoTest{
             new ActivityTestRule(MapsActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
-        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    public void newToiletView(){
+        onView(withId(R.id.map_view)).perform(longClick());
+        onView(withInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL)).perform(typeText("New Toilet"));
+        onView(withText("OK")).perform(click());
+        onView(withText("New Toilet")).check(matches(isDisplayed()));
     }
 }
